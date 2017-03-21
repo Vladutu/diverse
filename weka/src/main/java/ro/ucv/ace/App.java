f31b6f6fa52b3d92a543312a4bb6baf756b833a6
Vladutu <vladutu_georgian_4d@yahoo.com>
2017-03-21 20:09:35 +0200

    IR - added first homework

---
+++ weka/src/main/java/ro/ucv/ace/App.java
@@ -0,0 +1,11 @@
+package ro.ucv.ace;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        Classifier classifier = new Classifier();
+        classifier.classify();
+    }
+}
---
+++ weka/src/main/java/ro/ucv/ace/Classifier.java
@@ -0,0 +1,36 @@
+package ro.ucv.ace;
+
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
+
+import java.util.Random;
+
+/**
+ * Created by Geo on 21.03.2017.
+ */
+public class Classifier {
+
+    public void classify() throws Exception {
+        String path = getClass().getClassLoader().getResource("data.arff").getPath();
+        ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource(path);
+        Instances data = dataSource.getDataSet();
+        data.setClassIndex(4);
+        NaiveBayes naiveBayes = new NaiveBayes();
+        naiveBayes.buildClassifier(data);
+        Evaluation evaluation = new Evaluation(data);
+        evaluation.crossValidateModel(naiveBayes, data, 3, new Random());
+
+        System.out.println("Correct: " + evaluation.correct());
+        System.out.println("Incorrect: " + evaluation.incorrect());
+        System.out.println("Pct correct: " + evaluation.pctCorrect());
+        System.out.println("Pct incorrect: " + evaluation.pctIncorrect());
+        System.out.println("Kappa: " + evaluation.kappa());
+        System.out.println("Mean absolute error: " + evaluation.meanAbsoluteError());
+        System.out.println("Root mean square error: " + evaluation.rootMeanSquaredError());
+        System.out.println("Unclassified " + evaluation.unclassified());
+        System.out.println("Pct unclassified: " + evaluation.pctUnclassified());
+    }
+}
